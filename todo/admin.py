1050f9d1dfc6f716668d8045f6da07c701368937
marinaRomanchuk <marina.romanchuk23@gmail.com>
2021-11-09 01:33:42 +0300

    Changed models

--- todo/admin.py
+++ todo/admin.py
@@ -3,6 +3,6 @@ from .models import Todo
 # Register your models here.
 
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "completed")
+    list_display = ("title", "description", "completed", "start", "end", "remind_me")
 
 admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -5,6 +5,10 @@ class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     completed = models.BooleanField(default=False)
+    start = models.DateField()
+    end = models.DateField(blank=True)
+    remind_me = models.BooleanField(default=True)
+
 
     def __str__(self):
         return self.title
